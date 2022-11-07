89ee35df582dc2bd37aae14c5fee937588a6065b
Medarametla <289881@corpaa.aa.com>
2022-11-07 13:39:31 -0600

    initial commit

---
+++ Java/src/main/java/com/sasvi/modal/Student.java
@@ -0,0 +1,17 @@
+package com.sasvi.modal;
+
+public class Student {
+
+    private int number;
+    private String name;
+    private String university;
+
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
---
+++ Java/src/main/java/com/sasvi/programs/SutdentObjectsProgram.java
@@ -0,0 +1,85 @@
+package com.sasvi.programs;
+
+import com.sasvi.modal.Student;
+
+import java.util.List;
+
+public class SutdentObjectsProgram {
+
+    public static void main(String args[]) {
+        // create student objects. use the method - createStudents()
+
+        // print student information using iterator. use the method - printStudentsInfoUsingIterator
+
+        // print student information using iterator. use the method - printStudentsInfoUsingForEachLoop
+
+        // get student info by id. use the method - getStudent();
+
+        // filer students by name . Dont change the original list
+
+        // filer students by university . Dont change the original list.
+
+
+        // sort students by number
+
+        // sort students by age
+
+        // sort students by name
+
+        
+
+
+    }
+
+
+
+
+    private static List<Student> filterStudentsByName( List<Student> students, String name) {
+
+        return null;
+    }
+
+    private static List<Student> filterStudentsByUniversity( List<Student> students, String university) {
+
+        return null;
+    }
+
+    private static Student getStudent( List<Student> students, int number) {
+
+        return null;
+    }
+
+
+    private List<Student> sortStudentsByNumber(List<Student> students) {
+
+        return null;
+    }
+
+    private List<Student> sortStudentsByAge(List<Student> students) {
+
+        return null;
+    }
+
+    private List<Student> sortStudentsByName(List<Student> students) {
+
+        return null;
+    }
+
+
+
+    private static List<Student> createStudents() {
+        //Create 4 student objects
+
+        // add them to list
+       return null;
+    }
+
+    private static void printStudentsInfoUsingIterator(List<Student> students) {
+
+    }
+
+    private static void printStudentsInfoUsingForEachLoop(List<Student> students) {
+
+    }
+
+}
