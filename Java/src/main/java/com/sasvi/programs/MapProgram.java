3d7e30246f36e8a3a5f41356fef9fad1d5265106
Medarametla <289881@corpaa.aa.com>
2022-11-08 12:55:29 -0600

    Added map programs

---
+++ Java/src/main/java/com/sasvi/programs/MapProgram.java
@@ -0,0 +1,16 @@
+package com.sasvi.programs;
+
+public class MapProgram {
+
+    public static void main(String args[]) {
+
+        String name = "rama";
+
+        // print how many times each char is repeated
+        // output.......   r-1  a-2  m-1
+
+        // Use LinkedHashMap to solve the problem
+
+
+    }
+}
